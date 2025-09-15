972dce9e314493bc5d2a8d3c7e1c206f610ab6aa
luisf-csdev <105379183+luisf-csdev@users.noreply.github.com>
2025-09-15 23:14:21 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -7,6 +7,6 @@ module.exports = {
   ColorGray200: "#c1c1c4",
   ColorGray500: "#50505c",
   ColorGray800: "#1c1c1f",
-  ColorGray900: "#1e1e1e",
+  ColorGray900: "#121214",
   ColorPurple500: "#8257e6",
 };
